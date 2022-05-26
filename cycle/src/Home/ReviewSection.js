1b46f60817f4626bae379d434b95ec995734d695
jabin34 <isratjabinesha@gmail.com>
2022-05-26 16:42:17 +0900

    Contact section done

--- cycle/src/Home/ReviewSection.js
+++ cycle/src/Home/ReviewSection.js
@@ -24,10 +24,10 @@ useEffect(()=>{
  
   
     return (
-        <div>
+        <div className='my-24'>
            <h3 className='text-3xl p-5 '>Reviews</h3>
            <div class="divider mb-4"></div> 
-           <div className='grid  grid-cols-1  md:grid-cols-4 g-4 '>
+           <div className='grid  grid-cols-1  md:grid-cols-4 g-4 m-12 '>
                
               {reviewsdata.map((review)=><ReviewCard key={review._id} review={review}/>)}
            </div>
